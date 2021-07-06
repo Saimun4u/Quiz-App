3a824ef5d5f75179cc76170391e0b3c4dee3cf60
Saimun <saimun4u@gmail.com>
2021-07-06 00:02:53 -0500

    JS dom bubbling on showing answers

--- app.js
+++ app.js
@@ -2,19 +2,51 @@
 
 const questionContainer = document.getElementById('question-container');
 const startBtn = document.getElementById('start-btn')
+const questionElement = document.getElementById('question')
+const answerElement = document.getElementById('answer-buttons')
 
+//Shuffle questions
+
+let shuffleQuestions, currentQuestionIndex
 
 startBtn.addEventListener('click', startGame)
 
 function startGame(){
     questionContainer.classList.remove('hide')
     startBtn.classList.add('hide')
+    shuffleQuestions = questions.sort(()=> Math.random() - 0.5)
+    currentQuestionIndex = 0;
+    setNextQuestion();
 }
 
-function showNextQuestion(){
+function setNextQuestion(){
+    showQuestion(shuffleQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question){
+ questionElement.innerText = question.question;
+ question.answers.forEach(ans => {
+     const button = document.createElement('button')
+     button.innerText = ans.text
+     button.classList.add('btn')
+     if (ans.correct) {
+         button.dataset.correct = ans.correct
+     }
+     button.addEventListener('click', selectAnswer)
+     answerElement.appendChild(button)
+ })
+}
+
+function selectAnswer(e){
 
 }
 
-function showAnswer(){
-
-}
+questions = [
+    {
+        question: 'Which planet is closer to Earth?',
+        answers: [
+            {text: 'Venus', correct: true},
+            {text: 'Mars', correct: false}
+        ]
+    }
+]
